86d165f2dd1b7f2867469aa5140d25377ef09d1f
rahmed19 <ray.ahmed.sec@gmail.com>
2021-07-12 05:40:46 -0700

    firebase config setup

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,10 +4,15 @@ import 'firebase/auth'
 import FirebaseContext from '../context/firebase'
 
 const config = {
-
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: "instagram-clone-956ff.firebaseapp.com",
+    projectId: "instagram-clone-956ff",
+    storageBucket: "instagram-clone-956ff.appspot.com",
+    messagingSenderId: "748940838292",
+    appId: "1:748940838292:web:f321578668e3d3babc3b81"
 }
 
 const firebase = Firebase.initializeApp(config)
 const { FieldValue } = Firebase.firestore
-
-export { irebase, FieldValue }
+console.log('firebase', firebase)
+export { firebase, FieldValue }
