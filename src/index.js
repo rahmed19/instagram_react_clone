e53c63136aa9da03b3d43ea7957566c1ef0695a8
rahmed19 <ray.ahmed.sec@gmail.com>
2021-07-12 05:27:27 -0700

    firebase setup continued

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import FirebaseContext from './context/firebase'
+import { firebase, FieldValue } from './lib/firebase'
 
 ReactDOM.render(
   <FirebaseContext.Provider value={{ firebase, FieldValue }}>
