5ef43007ff05338810c2ae4829d53aaecc253e30
rahmed19 <ray.ahmed.sec@gmail.com>
2021-07-13 05:19:50 -0700

    lazy loading setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+Const Login = lazy(() => ('./pages/login'))
 
 function App() {
   return (
     <Router>
-
-    </Router>
+      <Suspense fallback={<p>Loading... </p>} >
+        <Switch>
+          <Route path="/login" component={Login} />
+        </Switch>
+      </Suspense>
+    </Router >
   );
 }
 
