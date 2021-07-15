8358959814eebbf314e792b4900a61c202bf8afa
rahmed19 <ray.ahmed.sec@gmail.com>
2021-07-15 04:49:59 -0700

    form setup

--- src/pages/login.js
+++ src/pages/login.js
@@ -22,19 +22,21 @@ export default function Login() {
             <div className="flex w-3/5">
                 <img src="/images/iphone-with-profile.jpg" className="max-w-md"></img>
             </div>
-            <h1 className="flex justify-center w-full">
-                <img src="/images/logo.png" alt="instagram" className="mt2 w-6/12"></img>
-            </h1>
-            {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+            <div className="flex flex-col w-2/5">
+                <h1 className="flex justify-center w-full">
+                    <img src="/images/logo.png" alt="instagram" className="mt2 w-6/12"></img>
+                </h1>
+                {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+                <form onSubmit={handleLogin} method="POST">
+                    <input
+                        aria-label="Enter your email address"
+                        type="text"
+                        placeholder="Email Address"
+                        className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary"
+                    />
+                </form>
+            </div>
 
-            <form onSubmit={handleLogin} method="POST">
-                <input
-                    aria-label="Enter your email address"
-                    type="text"
-                    placeholder="Email Address"
-                    className="text-sm text-gray-base"
-                />
-            </form>
         </div>
     )
 }
